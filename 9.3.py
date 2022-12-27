9d3205c215bf0796f53549e89f7b0d3596caf8c0
Myra Bui ALP (TM Master) <63907332+myrabui-alp@users.noreply.github.com>
2022-12-27 20:41:58 +0700

    9th

---
+++ 9.3.py
@@ -0,0 +1,7 @@
+# Get Only unique items from two sets
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+set1.update(set2)
+print(set1)
---
+++ 9.6.py
@@ -0,0 +1,6 @@
+# Return a set of elements present in Set A or B, but not both
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+set1.symmetric_difference_update(set2)
+print(set1)
